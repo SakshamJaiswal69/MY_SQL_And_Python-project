bf2713b289f0f5829108b14112694feab566c432
SakshamJaiswal69 <a6136593@gmail.com>
2025-05-28 02:31:48 -0700

    Create Ques_1.py

---
+++ Python GUI/Ques_1.py
@@ -0,0 +1,8 @@
+from tkinter import*
+
+screenroot=Tk()
+
+screen=Label(text= "Welcome to pycharm").pack() # in label attribute ther will be only name 
+screenroot.geometry("734x340")
+
+screenroot.mainloop()
